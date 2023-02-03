e9566c8d94f858adac5295f9b5b61ed07c551247
Avalynn <100826194+avalynndev@users.noreply.github.com>
2023-02-03 14:38:41 +0000

    1.0.1

---
+++ index.ts
@@ -0,0 +1,22 @@
+/** Modules **/
+
+export { flip } from "./src/flip";
+export { log } from "./src/log";
+export { print } from "./src/print";
+export { random } from "./src/random";
+export { uuid } from "./src/uuid";
+
+/** Util **/
+
+export { boolToNum } from "./src/util/boolToNum";
+export { boolToStr } from "./src/util/boolToStr";
+export { numToBool } from "./src/util/numToBool";
+export { numToStr } from "./src/util/numToStr";
+export { removeDuplicates } from "./src/util/removeDuplicates";
+export { strToBool } from "./src/util/strToBool";
+export { strToNum } from "./src/util/strToNum";
+
+/** DNS */
+
+export { a } from "./src/dns/a";
+export { cname } from "./src/dns/cname";
---
+++ src/dns/a.ts
@@ -0,0 +1,10 @@
+/**
+ * @function `validateARecord` - Validate an A record
+ * @returns { Boolean } - Returns true if valid, returns false if invalid
+ */
+
+export function a(record: string): boolean {
+    const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+    return regex.test(record);
+}
