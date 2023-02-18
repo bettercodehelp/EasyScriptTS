36e9735da3db4a5276155085fa432aa707766df5
fergqwertyui <fergusrobson2@gmail.com>
2023-02-18 12:09:46 +0000

    Refactor of code

--- src/util/isEmpty.ts
+++ src/util/isEmpty.ts
@@ -5,21 +5,21 @@
  */
 
 export function isEmpty(obj: object | any[]): boolean {
-    // Check if the object is null or undefined
-    if (obj == null) {
-      return true;
-    }
-  
-    // Check if the object is an array and has a length of 0
-    if (Array.isArray(obj) && obj.length === 0) {
-      return true;
-    }
-  
-    // Check if the object is an object and has no own properties
-    if (typeof obj === 'object' && Object.keys(obj).length === 0) {
-      return true;
-    }
-  
-    // Otherwise, the object is not empty
-    return false;
+  // Check if the object is null or undefined
+  if (obj == null) {
+    return true;
   }
+
+  // Check if the object is an array and has a length of 0
+  if (Array.isArray(obj) && obj.length === 0) {
+    return true;
+  }
+
+  // Check if the object is an object and has no own properties
+  if (typeof obj === "object" && Object.keys(obj).length === 0) {
+    return true;
+  }
+
+  // Otherwise, the object is not empty
+  return false;
+}
--- src/util/matches.ts
+++ src/util/matches.ts
@@ -4,52 +4,61 @@
  * @returns {boolean} returns true if the items do match, and false otherwise
  */
 
-
 export function matches(obj1: any, obj2: any): boolean {
     if (Array.isArray(obj1) && Array.isArray(obj2)) {
-      // If both inputs are arrays
-      if (obj1.length !== obj2.length) {
+        // If both inputs are arrays
+        if (obj1.length !== obj2.length) {
         return false;
-      }
-  
-      for (let i = 0; i < obj1.length; i++) {
+        }
+
+        for (let i = 0; i < obj1.length; i++) {
         // Recursively call matches only if the value is an object or an array
-        if ((typeof obj1[i] === "object" && obj1[i] !== null) || Array.isArray(obj1[i])) {
-          if (!matches(obj1[i], obj2[i])) {
+        if (
+            (typeof obj1[i] === "object" && obj1[i] !== null) ||
+            Array.isArray(obj1[i])
+        ) {
+            if (!matches(obj1[i], obj2[i])) {
             return false;
-          }
+            }
         } else if (obj1[i] !== obj2[i]) {
-          return false;
-        }
-      }
-      return true;
-    } else if (typeof obj1 === "object" && obj1 !== null && typeof obj2 === "object" && obj2 !== null) {
-      // If both inputs are objects
-      const keys1 = Object.keys(obj1);
-      const keys2 = Object.keys(obj2);
-  
-      if (keys1.length !== keys2.length) {
-        return false;
-      }
-  
-      for (const key of keys1) {
-        if (!keys2.includes(key)) {
-          return false;
-        }
-  
-        // Recursively call matches only if the value is an object or an array
-        if ((typeof obj1[key] === "object" && obj1[key] !== null) || Array.isArray(obj1[key])) {
-          if (!matches(obj1[key], obj2[key])) {
             return false;
-          }
-        } else if (obj1[key] !== obj2[key]) {
-          return false;
         }
-      }
-      return true;
+        }
+        return true;
+    } else if (
+        typeof obj1 === "object" &&
+        obj1 !== null &&
+        typeof obj2 === "object" &&
+        obj2 !== null
+    ) {
+        // If both inputs are objects
+        const keys1 = Object.keys(obj1);
+        const keys2 = Object.keys(obj2);
+
+        if (keys1.length !== keys2.length) {
+        return false;
+        }
+
+        for (const key of keys1) {
+        if (!keys2.includes(key)) {
+            return false;
+        }
+
+        // Recursively call matches only if the value is an object or an array
+        if (
+            (typeof obj1[key] === "object" && obj1[key] !== null) ||
+            Array.isArray(obj1[key])
+        ) {
+            if (!matches(obj1[key], obj2[key])) {
+            return false;
+            }
+        } else if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+        }
+        return true;
     } else {
-      // For all other cases, use strict equality
-      return obj1 === obj2;
+        // For all other cases, use strict equality
+        return obj1 === obj2;
     }
-  }
-  
+}
